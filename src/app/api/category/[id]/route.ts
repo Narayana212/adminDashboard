1f0492a15e155472363bc71468c8fb600f66f76b
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-09-12 15:52:21 +0530

    fixed the category form functionality

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -3,33 +3,33 @@ import { getAuth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET(request: NextRequest, { params }: { params: { id: number } }){
+export async function GET(request: NextRequest, { params }: { params: { id: number } }) {
     try {
-        const {userId}=getAuth(request)
-        if(!userId){
+        const { userId } = getAuth(request)
+        if (!userId) {
             return NextResponse.json({ message: "Unauthorized User" }, { status: 401 })
         }
         if (!params.id) {
             return NextResponse.json({ message: "Invalid category" }, { status: 400 })
         }
-        
+
         const category = await prisma.productCategory.findUnique({
             where: {
                 id: params.id
             }
         });
         return NextResponse.json({ message: category }, { status: 200 })
-        
-    } catch (error:any) {
+
+    } catch (error: any) {
         console.log(error.message)
         return NextResponse.json({ message: error.message }, { status: 500 })
-        
+
     }
 }
 
 
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: number } }){
+export async function DELETE(request: NextRequest, { params }: { params: { id: number } }) {
     try {
         const { userId } = getAuth(request);
         if (!userId) {
@@ -40,15 +40,15 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: n
             return NextResponse.json({ message: "Invalid category" }, { status: 400 })
         }
 
-        
 
-        let id =params.id
-        if(typeof params.id === "string"){
-             id = parseInt(params.id)
+
+        let id = params.id
+        if (typeof params.id === "string") {
+            id = parseInt(params.id)
         }
         const category = await prisma.productCategory.findUnique({
             where: {
-                id:id,
+                id: id,
             },
             include: {
                 products: true,
@@ -57,13 +57,13 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: n
         if (!category) {
             return NextResponse.json({ message: "Category not found" }, { status: 404 });
         }
-        if(category.products){
-            return NextResponse.json({ message:"This Category have Products"},{status: 400});
+        if (category.products.length > 0) {
+            return NextResponse.json({ message: "This Category have Products" }, { status: 400 });
         }
 
         await prisma.productCategory.delete({
             where: {
-                id:id
+                id: id
             }
         });
         return NextResponse.json({ message: "Deleted Succesfully" }, { status: 200 })
