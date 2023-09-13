089637c5ddf861c557711339fd5f4ef414d4f98e
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-09-13 18:28:17 +0530

    Changing Categories Search

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -6,15 +6,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
-        const { userId } = getAuth(request)
-        if (!userId) {
-            return NextResponse.json({ message: "UnAuthorizesd User" }, { status: 401 })
-        }
+
         const categories = await prisma.productCategory.findMany()
         return NextResponse.json({ message: categories }, { status: 200 })
 
 
     } catch (error: any) {
+        console.log(error.message)
         return NextResponse.json({ message: error.message }, { status: 500 })
 
     }
@@ -24,12 +22,9 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
     try {
-        const { userId } = getAuth(request)
         const requestBody = await request.json()
         const { category } = requestBody
-        if (!userId) {
-            return NextResponse.json({ message: "UnAuthorizesd User" }, { status: 401 })
-        }
+
         if (!category) {
             return NextResponse.json({ message: "Invalid Category" }, { status: 400 })
         }
@@ -39,17 +34,11 @@ export async function POST(request: NextRequest) {
                 name: category
             }
         })
-
-
         const data = await prisma.productCategory.findMany()
-
         return NextResponse.json({ message: data }, { status: 200 })
 
-
-
     } catch (error: any) {
+        console.log(error.message)
         return NextResponse.json({ message: error.message }, { status: 500 })
-
-
     }
 } 
