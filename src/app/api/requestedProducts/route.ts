9512b11dbadd00834e16c26be0bdc3c2811bccaf
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-09-25 22:51:19 +0530

    Small changes in depoly

--- src/app/api/requestedProducts/route.ts
+++ src/app/api/requestedProducts/route.ts
@@ -3,11 +3,17 @@ import { getAuth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 
+
+
 const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
+
+export async function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders });
+}
 
 export async function GET(request: NextRequest) {
     try {
