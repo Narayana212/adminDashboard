04dbb1684b75bb72be447bdca7f6b5ba712c2ad0
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-27 15:45:53 +0530

    first

--- src/app/(root)/notAdmin/page.tsx
+++ src/app/(root)/notAdmin/page.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Heading from '@/components/ui/heading'
+
+import { useAuth } from '@clerk/nextjs';
 import React from 'react'
 
 export default function NotAdmin() {
+  const { userId } = useAuth();
   return (
     <div className='w-screen h-screen flex justify-center items-center -mt-24'>
         <Heading text='Your Not Admin'/>
+        <p>{userId}</p>
     </div>
   )
 }
