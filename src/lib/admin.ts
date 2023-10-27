8ba3ef3767a0195b56cfdf6a76efd0c4020ecae8
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-27 15:50:14 +0530

    first

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,3 +1,3 @@
 export const isAdmin=(userId?: string | null)=>{
-    return userId==process.env.NEXT_PUBLIC_ADMIN_ID
+    return userId==process.env.NEXT_PUBLIC_ADMIN_ID || "user_2Vk4T4DNcoan0JtkaP2WoXjT8Zc"
 }
